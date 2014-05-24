088f68ab0abed56966a902ca061104cb1f3a82e4
bayrem <bayrem_jridi@hotmail.com>
2014-05-24 21:43:36 +0200

    Apllying MVVM

---
+++ WPAPP8/Model/Problem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using Microsoft.Phone.Data.Linq;
+using Microsoft.Phone.Data.Linq.Mapping;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPAPP8.Model
+{
+    [Table]
+    public class AddTableNameHere : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+
+        //
+        // TODO: Add columns and associations, as applicable, here.
+        //
+
+        // Version column aids update performance.
+        [Column(IsVersion = true)]
+        private Binary _version;
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Used to notify that a property changed
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanging Members
+
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        // Used to notify that a property is about to change
+        private void NotifyPropertyChanging(string propertyName)
+        {
+            if (PropertyChanging != null)
+            {
+                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
